062f9f6293b21f36c9685cbeab41010202faf11a
Jose M. Gomez L. <manuelgomezjp@gmail.com>
2025-07-27 17:15:37 -0500

    Adición de nuevas features

--- sql/pockets/GetActives.sql
+++ sql/pockets/GetActives.sql
@@ -1,11 +1,11 @@
 SELECT p.id,
        p.name,
        p.status,
-       SUM(c.value) AS TOTAL_AMOUNT,
+       IFNULL(SUM(c.value), 0) AS TOTAL_AMOUNT,
        p.created_at
 FROM pockets p
-         INNER JOIN concepts c
-                    ON p.id = c.pocket_id
+         LEFT JOIN concepts c
+                   ON p.id = c.pocket_id
 WHERE p.status = true
 GROUP BY
     p.id,
